597fdbb68aabe93079ceff30cacf1cb41d28c2e4
SilaevAlex <dp110393sad@gmail.com>
2022-03-21 11:22:15 +0200

    add DOM HW

---
+++ main.js
@@ -0,0 +1,29 @@
+"use strict"
+
+var user = {
+    id: 777,
+    name: "Vasa",
+    age: 77,
+    email: "vasa007@gmail.com",
+    city: "Kiev",
+
+}
+
+user.dataCard = function () {
+
+    var userInfo = document.querySelector("#user-block");
+
+    var info = "";
+
+    for (var data in this) {
+        if (data!= "dataCard")
+            info+=`${data} ${this[data]} `;
+    }
+
+    
+
+    userInfo.innerHTML = info;
+
+};
+
+user.dataCard();
